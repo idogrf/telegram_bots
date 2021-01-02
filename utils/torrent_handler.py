059298fa03cabb6759bf6f53d33753189fd9a096
idogrf <idogreenfeld@gmail.com>
2021-01-02 19:56:25 +0200

    refactor torrent operations to different repo

--- utils/torrent_handler.py
+++ utils/torrent_handler.py
@@ -2,8 +2,9 @@ import sys
 import subprocess
 from telepot.helper import Sender
 
-sys.path.append('/home/pi/projects/cronjobs')
-from download_torrents import run_torrent_download, delete_small_dirs, get_free_usb_space, get_free_root_space
+sys.path.append('/home/pi/projects/repositories/torrent_tools')
+from torrent_tools.data_operations import delete_small_dirs, get_free_root_space, get_free_media_space
+from torrent_tools.torrent_operations import refresh_rss_torrents, download_torrent, stop_all_torrents, start_all_torrents, list_torrents
 
 from utils.handler import Handler
 
@@ -35,9 +36,9 @@ class TorrentHandler(Handler):
     def _run_command_refresh(self):
         """ Refresh torrents RSS feed """
         self._sender.sendMessage('Refreshing torrents list.. please wait...')
-        output = run_torrent_download()
+        response = refresh_rss_torrents()
         self._sender.sendMessage('Finished refreshing torrents. Results - ')
-        self._sender.sendMessage(output)
+        self._sender.sendMessage(response)
 
     def _run_command_download_torrent(self):
         """ Download new torrent """
@@ -52,56 +53,38 @@ class TorrentHandler(Handler):
     def _run_command_purge_dirs(self):
         """ Delete empty torrent folders """
         self._sender.sendMessage('Removing empty dirs')
-        deleted_dirs = delete_small_dirs()
-        if len(deleted_dirs) == 0:
-            self._sender.sendMessage('No dirs found for deletion')
-        else:
-            self._sender.sendMessage(f'Dirs deleted - {deleted_dirs}')
+        response = delete_small_dirs()
+        self._sender.sendMessage(response)
 
     def _run_command_free_space(self):
         """ Check free disk space """
-        usb_space = get_free_usb_space()
+        media_space = get_free_media_space()
         root_space = get_free_root_space()
-        self._sender.sendMessage(f'Free disk space on USB - {usb_space:.1f}GB')
+        self._sender.sendMessage(f'Free disk space on media mount - {media_space:.1f}GB')
         self._sender.sendMessage(f'Free disk space on root - {root_space:.1f}GB')
 
     def _run_command_start_all(self):
         """ Start all torrents """
         self._sender.sendMessage('Starting all torrents')
-        self._send_to_transmission(['--torrent', 'all', '--start'])
+        response = start_all_torrents()
+        self._sender.sendMessage(response)
 
     def _run_command_stop_all(self):
         """ Stop all active torrents """
         self._sender.sendMessage('Stopping all torrents')
-        self._send_to_transmission(['--torrent', 'all', '--stop'])
+        response = stop_all_torrents()
+        self._sender.sendMessage(response)
+
+    def _run_command_list(self):
+        """ List all active torrents """
+        response = list_torrents()
+        self._sender.sendMessage(response)
 
     # Class helper methods
     def _download_torrent(self):
-        self._sender.sendMessage(f'Starting transmission torrent client')
-        params = ['--add', self._input_params[0], '--download-dir', self._input_params[1]]
-        self._send_to_transmission(params)
-
-    def _send_to_transmission(self, params):
-        p = subprocess.Popen(['transmission-remote', '--authenv'] + params, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, err = p.communicate()
-
-        output = output.decode("utf-8")
-        err = err.decode("utf-8")
-
-        print(output)
-        print(err)
-
-        if 'success' in output.lower() and len(err) == 0:
-            self._sender.sendMessage(f'Success!')
-            self._sender.sendMessage(f'You can check the progress by going to http://10.0.0.17:9091')
-        else:
-            self._sender.sendMessage(f'Failed')
-
-        if len(output) > 0:
-            self._sender.sendMessage(f'Output message - {output}')
-
-        if len(err) > 0:
-            self._sender.sendMessage(f'Output message - {err}')
+        self._sender.sendMessage(f'Downloading torrent')
+        response = download_torrent(self._input_params[0], self._input_params[1])
+        self._sender.sendMessage(response)
 
     def _input_param(self, msg):
         self._input_params.append(msg)
