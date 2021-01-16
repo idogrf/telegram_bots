27b7e2f8ceafc1147a46f5c7ef976c18e6270504
idogrf <idogreenfeld@gmail.com>
2021-01-16 12:21:27 +0200

    add subscription updater

---
+++ telegram_subscribers_update.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+import telepot
+
+
+class UpdaterBot:
+    def __init__(self):
+        bot_token = os.getenv('TEL_BOT_TOKEN')
+        subscribed_chats_file = os.getenv('SUBSCRIBED_CHATS_FILE')
+        with open(subscribed_chats_file, 'r') as f:
+            subscribed_chats = json.load(f)
+
+        self._subscribed_chat_ids = subscribed_chats['chat_ids']
+        self._bot = telepot.Bot(bot_token)
+
+    def update_subscribers(self, msg):
+        for chat_id in self._subscribed_chat_ids:
+            self._bot.sendMessage(chat_id, msg)
